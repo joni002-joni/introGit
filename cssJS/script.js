7539c5520e48273895fe6f98d9662e33ced3e7a1
Januat <jonimustafa002@gmail.com>
2024-10-31 09:04:15 +0300

    Типы өзгерту бастамасы

--- cssJS/script.js
+++ cssJS/script.js
@@ -1,3 +1,28 @@
-console.log("Javascript worddd");
+// console.log("Javascript worddd");
 // java //
-console.log("Mystafaev Janyat");
+// console.log("Mystafaev Janyat");
+
+// var name = "Amjlit Cyber School"
+//  console.log(name)
+
+  
+//  let name = "Amjlit Cyber School"
+
+//  if (true){
+//     let name = "Janyat"
+//     console.log("Условиянын ишиндеги аты:" + name);
+//   }
+
+//    console.log("Жалпы аты:" + name)
+
+// const name = "janyat"
+ 
+// name="Miras"
+
+// console.log("ьенин атым" + name)
+// console.log("my name is" + name)
+
+const name="Janyat"
+let age = 5
+let age2= 10
+console.log(age+age2)

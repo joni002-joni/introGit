faf78bb7b63cd3700fc78cf701564768cde49c17
Januat <jonimustafa002@gmail.com>
2024-11-04 08:43:04 +0300

    Үй жұмысы №4

---
+++ jss/script.js
@@ -0,0 +1,41 @@
+// косу
+//let a = 5, b = 3, sum
+ 
+// 
+//let z = 10 
+//let y = 7 
+//sum = 20 % 7 //6 қалдық
+
+//z++; //z= z+1
+//z--; //z= z-1
+
+ // z+=2 //z = z+2 z=20+2
+  //z-=5 //z = z-5 z=22-5
+  //z+=5 //z = z*5 z=17*5
+//  z+=5 //z = z/5 z=85/5
+//  z+=5 //z = z%5 z=17%5        
+
+
+// console.log(" z * y = " + (z*y));
+// console.log(" a * b = " + (a*b));
+
+// console.log(" z % y = " + sum);     
+
+   // var num = prompt("Ввудите число")
+
+   //var kobeitindi = num * num //квадратын табатын айнымалы
+
+  //alert(" a * b = " + kobeitindi)
+
+  let number1 = Number(prompt("Ввудите первое число:"));
+
+  let num1 = Number (prompt("Сан енгізіңіз:"));
+
+  let squaree = number1 * number1
+
+  let square = num1 * num1
+
+  alert("Ұзындығы" + square +'\n'+ "ені" + squaree )
+
+ 
+
